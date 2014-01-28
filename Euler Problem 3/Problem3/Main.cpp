5bef3ff3c64e6bf93d288d7a846148df3fefaab5
Joesph Olliff <joeolliff@gmail.com>
2014-01-28 13:35:54 -0500

    Got it to work effeciently

--- Euler Problem 3/Problem3/Main.cpp
+++ Euler Problem 3/Problem3/Main.cpp
@@ -4,91 +4,43 @@ using namespace std;
 int main()
 {
 	//Declare Variables
-	long array1[100000];
-	long array2[100000];
 	long long count = 0;
 	long long count2 = 0;
-	long long count3 = 0;
 	long long num1 = 0;
-	long long temp = 0;
-	long long temp2 = 0;
-	long long temp3 = 0;
+	long long temp = 0; 
 	long long factor = 600851475143;
-
-	//Initialize arrays
-	for (count = 0; count < 100000; count++)
-	{
-		array1[count] = 0;
-	}
+	bool notPrime = false;
 	
-	for (count = 0; count < 100000; count++)
+	//Find the largest Prime
+	for (count = 2; count < factor; count++)
 	{
-		array2[count] = 0;
-	}
-
-	cout << "Made it past Initializeing " << endl;
-
-	//Generate all of the factors
-	for (count = 0; count < factor; count++)
-	{
-		for (count2 = 0; count2 < factor; count2++)
+		if (factor % count == 0)
 		{
-			cout << "progress 1: " << count << " progress 2: " << count2 << endl;
-			if (count * count2 == factor)
+			temp = count;
+			cout << "Found a factor: " << factor/temp << endl;
+			for (count2 = 2; count2 < factor/temp; count2++)
 			{
-				array1[temp] = count;
-				temp++;
-				array1[temp] = count2;
-				temp++;
-			}
-		}
-	}
-
-	cout << "Made it past Generating all of the factors " << endl;
-
-	//Determine which factors are Prime
-	for (count = 0; count < temp; count++)
-	{
-		if (array1[count] == 1)
-		{
-			array2[temp2] = array1[count];
-			temp2++;
-		}
-		else
-		{
-			for (count2 = 0; count2 < array1[count]; count2++)
-			{
-				for (count3 = 0; count3 < array1[count]; count3++)
+				if (factor/temp % count2 == 0)
 				{
-					if (count2 * count3 == array1[count])
-					{
-						temp3++;
-					}
+					cout << "It's not a prime..." << endl;
+					notPrime = true;
+					break;
 				}
 			}
-			if (temp3 == 1)
+			if (!notPrime)
 			{
-				array2[temp2] = array1[count];
-				temp2++;
-				temp3 = 0;
+				cout << "Found it!" << endl;
+				system("pause");
+				num1 = factor/temp;
+				break;
 			}
 			else
 			{
-				temp3 = 0;
+				notPrime = false;
 			}
 		}
 	}
-
-	cout << "Made it past Determining which factors are Prime " << endl;
-
-	//Determine which of the Primes is the largest
-	for (count = 0; count < temp2; count++)
-	{
-		if (num1 < array2[count])
-		{
-			num1 = array2[count];
-		}
-	}
+	system("cls");
 
 	//Display the results
 	cout << "The largest Prime Factor of: " << factor << " is: " << num1 << endl << endl;
